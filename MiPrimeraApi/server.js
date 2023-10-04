8105fc251c5a5701ee0d32698f65697c6e071e57
FernandoEmilianoFloresDeLaRiva <emilianoflores07081@gmail.com>
2023-10-04 12:58:13 -0600

    .gitignore se añade

--- MiPrimeraApi/server.js
+++ MiPrimeraApi/server.js
@@ -1,30 +1,30 @@
-const express = require('express')
-const mysql = require('mysql2')
-const myconn = require('express-myconnection')
-const routes = require('./routes')
+const express = require("express");
+const mysql = require("mysql2");
+const myconn = require("express-myconnection");
+const routes = require("./routes");
 
 const db = {
-    host: 'localhost',
-    port : 3306,
-    user: 'root',
-    password: 'Superm@n0708',
-    database: 'datos_login',
-}
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
+  port: 3306,
+};
 
 const app = express();
-app.set('port', process.env.PORT || 9000)
+app.set("port", process.env.PORT || 9000);
 
 //middlware
-app.use(myconn(mysql, db, 'single'))
-app.use(express.json())
+app.use(myconn(mysql, db, "single"));
+app.use(express.json());
 
 //routes
-app.get('/', (req, res) => {
-    res.send('Bienvenido a la API :)')
-})
-app.use('/api', routes)
+app.get("/", (req, res) => {
+  res.send("Bienvenido a la API :)");
+});
+app.use("/api", routes);
 
 //Verificacion que el servidor esta corriendo
 app.listen(9000, () => {
-    console.log('Servidor corriendo en puerto ', app.get('port'))
-})
+  console.log("Servidor corriendo en puerto ", app.get("port"));
+});
